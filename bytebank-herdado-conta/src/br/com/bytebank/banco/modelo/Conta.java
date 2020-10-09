0ea2de3968c8f982591c7955c8cf4e893b49d3f6
Wesley <wesley.contato@hotmail.com>
2020-10-08 22:10:08 -0300

    O método equals

--- bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
@@ -1,6 +1,5 @@
 package br.com.bytebank.banco.modelo;
 
-
 /**
  * Classe representa a moldura de uma conta
  * 
@@ -9,90 +8,105 @@ package br.com.bytebank.banco.modelo;
  */
 public abstract class Conta extends Object {
 
-    protected double saldo;
-    private int agencia;
-    private int numero;
-    private Cliente titular;
-    private static int total = 0;
-    
-    /**
-     * Construtor para inicializar o objeto Conta a partir da agencia e numero.
-     * 
-     * @param agencia
-     * @param numero
-     */
-    public Conta(int agencia, int numero){
-        Conta.total++;
-        //System.out.println("O total de contas é " + Conta.total);
-        this.agencia = agencia;
-        this.numero = numero;
-        //this.saldo = 100;
-        //System.out.println("Estou criando uma conta " + this.numero);
-    }
+	protected double saldo;
+	private int agencia;
+	private int numero;
+	private Cliente titular;
+	private static int total = 0;
 
-    public abstract void deposita(double valor);
+	/**
+	 * Construtor para inicializar o objeto Conta a partir da agencia e numero.
+	 * 
+	 * @param agencia
+	 * @param numero
+	 */
+	public Conta(int agencia, int numero) {
+		Conta.total++;
+		// System.out.println("O total de contas é " + Conta.total);
+		this.agencia = agencia;
+		this.numero = numero;
+		// this.saldo = 100;
+		// System.out.println("Estou criando uma conta " + this.numero);
+	}
 
-    /**
-     * Valor precisa ser maior do que o saldo.
-     * 
-     * @param valor
-     * @throws SaldoInsuficienteException
-     */
-    public void saca(double valor) throws SaldoInsuficienteException{
-    	
-        if(this.saldo < valor) {
-            throw new SaldoInsuficienteException("Saldo: " + this.saldo + ", Valor: " + valor);
-        } 
-        
-        this.saldo -= valor;       
-    }
+	public abstract void deposita(double valor);
 
-    public void transfere(double valor, Conta destino) throws SaldoInsuficienteException{
-        this.saca(valor);
-        destino.deposita(valor);
-    }
+	/**
+	 * Valor precisa ser maior do que o saldo.
+	 * 
+	 * @param valor
+	 * @throws SaldoInsuficienteException
+	 */
+	public void saca(double valor) throws SaldoInsuficienteException {
 
-    public double getSaldo(){
-        return this.saldo;
-    }
+		if (this.saldo < valor) {
+			throw new SaldoInsuficienteException("Saldo: " + this.saldo + ", Valor: " + valor);
+		}
 
-    public int getNumero(){
-        return this.numero;
-    }
+		this.saldo -= valor;
+	}
 
-    public void setNumero(int numero){
-        if(numero <= 0) {
-            System.out.println("Nao pode valor menor igual a 0");
-            return;
-        }
-        this.numero = numero;
-    }
+	public void transfere(double valor, Conta destino) throws SaldoInsuficienteException {
+		this.saca(valor);
+		destino.deposita(valor);
+	}
 
-    public int getAgencia(){
-        return this.agencia;
-    }
+	public double getSaldo() {
+		return this.saldo;
+	}
 
-    public void setAgencia(int agencia){
-       if(agencia <= 0) {
-           System.out.println("Nao pode valor menor igual a 0");
-           return;
-       }
-       this.agencia = agencia;
-    }
+	public int getNumero() {
+		return this.numero;
+	}
 
-    public void setTitular(Cliente titular){
-        this.titular = titular;
-    }
+	public void setNumero(int numero) {
+		if (numero <= 0) {
+			System.out.println("Nao pode valor menor igual a 0");
+			return;
+		}
+		this.numero = numero;
+	}
 
-    public Cliente getTitular(){
-        return this.titular;
-    }
+	public int getAgencia() {
+		return this.agencia;
+	}
 
-    public static int getTotal(){
-        return Conta.total;
-    }
-    
-    @Override
+	public void setAgencia(int agencia) {
+		if (agencia <= 0) {
+			System.out.println("Nao pode valor menor igual a 0");
+			return;
+		}
+		this.agencia = agencia;
+	}
+
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+
+	public Cliente getTitular() {
+		return this.titular;
+	}
+
+	public static int getTotal() {
+		return Conta.total;
+	}
+
+	@Override
+	public boolean equals(Object ref) {
+		
+		Conta outra = (Conta) ref;
+		
+		if (this.agencia != outra.agencia) {
+			return false;
+		}
+		if (this.numero != outra.numero) {
+			return false;
+		}
+
+		return true;
+	}
+
+	@Override
 	public String toString() {
 		return "Numero: " + this.numero + ", Agencia: " + this.agencia;
 	}
